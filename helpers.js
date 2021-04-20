790821f6885f8c12292c9f2916475ae68fdbf601
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-04-20 11:55:41 +0930

    fix: Correct arrow function sytax error

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
 // Capitalize the first letter of each word in a given string
-exports.capitalizeString(str) {
+exports.capitalizeString = (str) => {
   return str.replace(/\b\w/g, (char) => char.toUpperCase());
-}
+};
