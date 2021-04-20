0cf7bc5505f36d77efcec8041465c3578883cfdd
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-04-20 12:12:40 +0930

    feat: Implement entity operations menu

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const utils = require("./helpers");
 
 // Render Main Menu
 function showMainMenu() {
+  console.clear();
+
   inquirer
     .prompt([
       {
@@ -21,15 +24,43 @@ function showMainMenu() {
     .then((answers) => {
       // If user selects 'Exit' option, terminate the process
       if (answers.mainMenu === 0) {
-        console.log("\n" + "Thank you for using Employee Tracker!");
+        console.clear();
+        console.log(
+          "Exiting Application..." +
+            "\n" +
+            "Thank you for using Employee Tracker!"
+        );
         process.exit();
       }
+      console.clear();
       // For all other options, run showEntitymenu() with user's selected choice value
       showEntityMenu(answers.mainMenu);
     });
 }
 
 // Render Entity Options Menu
-function showEntityMenu(entityName) {}
+function showEntityMenu(entityName) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: `${entityName}Methods`,
+        message: "What would you like to do?",
+        choices: [
+          `View ${utils.capitalizeString(entityName)}s`,
+          `Add New ${utils.capitalizeString(entityName)}`,
+          new inquirer.Separator(),
+          { name: "< Go Back", value: 0 },
+        ],
+      },
+    ])
+    .then((res) => {
+      const answer = res[Object.keys(res)[0]];
+      if (answer === 0) {
+        console.clear();
+        showMainMenu();
+      }
+    });
+}
 
 showMainMenu();
