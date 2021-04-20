d5f6b409bf462d059607757c5367cbdfd80ced84
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-04-20 11:50:56 +0930

    feat: Implement main menu prompts

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
-function showMenu() {}
+// Render Main Menu
+function showMainMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mainMenu",
+        message: "Main Menu",
+        choices: [
+          { name: "Departments", value: "department" },
+          { name: "Roles", value: "role" },
+          { name: "Employees", value: "employee" },
+          new inquirer.Separator(),
+          { name: "Exit", value: 0 },
+        ],
+      },
+    ])
+    .then((answers) => {
+      // If user selects 'Exit' option, terminate the process
+      if (answers.mainMenu === 0) {
+        console.log("\n" + "Thank you for using Employee Tracker!");
+        process.exit();
+      }
+      // For all other options, run showEntitymenu() with user's selected choice value
+      showEntityMenu(answers.mainMenu);
+    });
+}
+
+// Render Entity Options Menu
+function showEntityMenu(entityName) {}
+
+showMainMenu();
